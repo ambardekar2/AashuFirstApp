4721d35d84288aa01dd5cc532df8c97d34109d26
ambardekar2 <aashutosh.ambardekar@gmail.com>
2026-04-25 13:50:22 -0700

    second commit

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Aashutosh</Text>
+      <Text>How are you doing?</Text>
       <StatusBar style="auto" />
     </View>
   );
